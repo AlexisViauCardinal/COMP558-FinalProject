027fc42fb8fd5f98268d55828fd88685eeb59cd6
Rafid Saif <73025878+rsaif22@users.noreply.github.com>
2024-12-12 11:39:02 -0500

    Create ShiTomasi.py

---
+++ code/feature_detection/ShiTomasi.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2 as cv
+from FeatureDetector import FeatureDetector
+
+def array_from_keypoints(kp):
+    return np.array([k.pt for k in kp], dtype=np.float32)
+
+class ShiTomasiDetector(FeatureDetector):
+    def __init__(self, maxCorners: int = 100, qualityLevel: float = 0.01, minDistance: float = 10):
+        self.maxCorners = maxCorners
+        self.qualityLevel = qualityLevel
+        self.minDistance = minDistance
+
+    def detect_features(self, image: np.ndarray) -> np.ndarray:
+        # If the image is not in grayscale, convert it
+        if len(image.shape) > 2:
+            image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+        kp = cv.goodFeaturesToTrack(image, self.maxCorners, self.qualityLevel, self.minDistance)
+        feature_array = np.array([k.pt for k in kp], dtype=np.float32)
+        return feature_array
